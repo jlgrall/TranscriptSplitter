58ea782c25d4c5608f6b838716c37c4f316f49ec
jlgrall <jlgrall@users.noreply.github.com>
2016-01-07 15:08:50 +0100

    Add gadget create and destroy events

--- gadget.js
+++ gadget.js
@@ -11,18 +11,23 @@
 				// Call method for each element:
 				for(var i = 0; i < this.length; i++) {
 					var elem = this[i],
+						$elem,
 						instance = $.data(elem, gadgetFullName),	// Get the instance for element
 						args,
 						destroy = methodName === "destroy";
 					
 					// Create instance if needed:
 					if(!instance && !destroy) {
+						$elem = $(elem);
 						args = Array_slice.call(arguments);
-						args.unshift(undefined, $(elem));
+						args.unshift(undefined, $elem);
 						// From: http://stackoverflow.com/questions/1606797/use-of-apply-with-new-operator-is-this-possible/8843181#8843181
 						// "Equivalent" to: new (Constr.apply(args))
 						instance = new (Function.prototype.bind.apply(Constr, args));
-						$.data(elem, gadgetFullName, instance);
+						$elem.data(gadgetFullName, instance);
+						var eventData = {gadgetName: gadgetName, instance: instance};
+						$elem.trigger(gadgetName + ":create", eventData);
+						$elem.trigger("gadget:create", eventData);
 					}
 					else if(instance && (!destroy || instance[methodName])) {
 						// Check method name and get method arguments:
@@ -38,7 +43,13 @@
 						}
 					}
 					
-					if(destroy && instance) $.removeData(elem, gadgetFullName);
+					if(destroy && instance) {
+						$elem = $(elem);
+						var eventData = {gadgetName: gadgetName, instance: instance};
+						$elem.removeData(gadgetFullName);
+						$elem.trigger(gadgetName + ":destroy", eventData);
+						$elem.trigger("gadget:destroy", eventData);
+					}
 				}
 			
 				return this;	// Chaining
