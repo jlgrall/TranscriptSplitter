cd056fcee27e4d11154044a1f2ff26c189f722f2
jlgrall <jlgrall@users.noreply.github.com>
2016-01-11 16:31:34 +0100

    Add storeState to automatically save inputs

--- transcriptsaver.js
+++ transcriptsaver.js
@@ -21,20 +21,24 @@
 			
 			this.debounceSaveProgressBound = this.debounceSaveProgress.bind(this);
 			
+			this.storeState = {};
+			
+			var self = this;
 			this.onTranscriptChanged = function() {
-				if(this.playerGadget) this.lastCurrentTime = this.playerGadget.player.currentTime;
-				this.lastTranscriptChange = Date.now();
+				if(self.playerGadget) self.lastCurrentTime = self.playerGadget.player.currentTime;
+				self.lastTranscriptChange = Date.now();
 				//console.log("Transcript changed");
-				if(this.debounceTimeout === -1) this.debounceSaveProgress();
-			}.bind(this);
-			this.onPlaybackRateChanged = function(e, data) {
-				this.savePlaybackRate();
-			}.bind(this);
+				if(self.debounceTimeout === -1) self.debounceSaveProgress();
+			};
 			this.onunload = function() {
-				if(this.debounceTimeout !== -1) {
-					this.saveProgress();
+				if(self.debounceTimeout !== -1) {
+					self.saveProgress();
 				}
-			}.bind(this);
+			};
+			
+			$element.on("change", ".storeState", function(e) {
+				self.saveStoreStateFor(e.target);
+			});
 			
 			$(window).on("unload.transcriptSaver", this.onunload);
 			
@@ -57,8 +61,6 @@
 			if(this.playerGadget) options.$transcriptPlayer.off(".transcriptSaver");
 			options.$transcriptPlayer = $transcriptPlayer;
 			this.playerGadget = $transcriptPlayer ? options.$transcriptPlayer.data("gadget-transcriptPlayer") : undefined;
-			
-			if(this.playerGadget) $transcriptPlayer.on("playbackRateChanged", this.onPlaybackRateChanged);
 		},
 		setTranscriptWriter: function($transcriptWriter) {
 			var options = this.options;
@@ -116,21 +118,6 @@
 				}
 			}
 		},
-		savePlaybackRate: function() {
-			if(this.playerGadget) {
-				this.options.store.set("state.playbackRate", this.playerGadget.playbackRate());
-				this.$element.trigger("playbackRateSaved");
-			}
-		},
-		loadPlaybackRate: function() {
-			if(this.playerGadget) {
-				var playbackRate = parseFloat(this.options.store.get("state.playbackRate"));
-				if(!isNaN(playbackRate)) {
-					this.playerGadget.playbackRate(playbackRate);
-					this.$element.trigger("playbackRateLoaded");
-				}
-			}
-		},
 		savePlayerSource: function() {
 			this.options.store.set("state.playerURLInput", this.options.$playerURLInput.val());
 		},
@@ -145,15 +132,154 @@
 				}
 			}
 		},
+		_loadStoreStateKey: function(type, key) {
+			var data = JSON.parse(this.options.store.get("state." + type));
+			if(!this.storeState[type]) this.storeState[type] = {};
+			this.storeState[type][key] = data && data[key];
+		},
+		_getStoreStateInfoFor: function(elem, getVal) {
+			var $elem = $(elem),
+				infos = {elem: elem, $elem: $elem};
+			
+			if($elem.is("input[type=checkbox]")) {
+				infos.type = "checkboxes";
+				infos.key = $elem.prop("id");
+			}
+			else if($elem.is("input[type=radio]")) {
+				infos.type = "radios";
+				infos.key = $elem.prop("name");
+			}
+			else if($elem.is("select:not([multiple])")) {
+				infos.type = "selects";
+				infos.key = $elem.prop("id");
+			}
+			else {
+				console.log("storeState not implemented for:", elem);
+			}
+			
+			if(infos.type && getVal) this._getStoreStateInfosValFor(infos);
+			
+			return infos;
+		},
+		_getStoreStateInfosValFor: function(infos) {
+			var type = infos.type,
+				key = infos.key,
+				$element = this.$element,
+				$target;
+			
+			if(type) {
+				if(type === "checkboxes") {
+					infos.val = infos.$elem.is(":checked") || undefined;
+				}
+				else if(type === "radios") {
+					$target = $element.find('input[type=radio][name="' + key + '"]:checked');
+					infos.val = $target.val();
+				}
+				else if(type === "selects") {
+					$target = $element.find("select#" + key + " option:selected");
+					infos.val = $target.val();
+				}
+			}
+			return infos;
+		},
+		saveStoreStateVal: function(type, key, val) {
+			var storeState = this.storeState;
+			if(!storeState[type]) storeState[type] = {};
+			if(storeState[type][key] !== val) {
+				if(val === undefined) delete storeState[type][key];
+				else storeState[type][key] = val;
+				this.options.store.set("state." + type, JSON.stringify(storeState[type]));
+			}
+		},
+		saveStoreStateFor: function(elem) {
+			var infos = this._getStoreStateInfoFor(elem, true),
+				type = infos.type;
+			
+			if(type) {
+				this.saveStoreStateVal(type, infos.key, infos.val);
+				this.$element.trigger(infos.key + "Saved");
+			}
+		},
+		saveAllStoreStates: function() {
+			var self = this;
+			this.$element.find(".storeState").each(function(i, elem) {
+				self.saveStoreStateFor(elem);
+			});
+		},
+		loadStoreStateVal: function(type, key) {
+			this._loadStoreStateKey(type, key);
+			
+			var $element = this.$element,
+				storeState = this.storeState,
+				storedVal = storeState[type] ? storeState[type][key] : undefined,
+				$target, infos;
+			
+			if(type === "checkboxes") {
+				$target = $element.find("input[type=checkbox]#" + key);
+				infos = this._getStoreStateInfoFor($target[0], true);
+				if(storedVal !== infos.val) $target.prop('checked', !!storedVal).change();
+			}
+			else if(type === "radios") {
+				$target = $element.find('input[type=radio][name="' + key + '"]');
+				infos = this._getStoreStateInfoFor($target[0], true);
+				if(storedVal !== infos.val) {
+					if(storedVal !== undefined && storedVal !== false) {
+						$target.filter('[value="' + storedVal + '"]').prop('checked', true).change();
+					}
+					else {
+						$target.filter(":checked").prop('checked', false).change();
+					}
+				}
+			}
+			else if(type === "selects") {
+				$target = $element.find("select#" + key);
+				infos = this._getStoreStateInfoFor($target[0], true);
+				if(storedVal !== infos.val) {
+					if(storedVal !== undefined && storedVal !== false) {
+						$target.find('option[value="' + storedVal + '"]').prop('selected', true);
+						$target.change();
+					}
+					else {
+						$target.find("option:selected").prop('selected', false);
+						$target.change();
+					}
+				}
+			}
+			else {
+				console.log("loadStoreStateVal not implemented for type: " + type);
+			}
+		},
+		loadStoreStateFor: function(elem) {
+			var infos = this._getStoreStateInfoFor(elem);
+			
+			if(infos.type) {
+				this.loadStoreStateVal(infos.type, infos.key);
+				this.$element.trigger(infos.key + "Loaded");
+			}
+		},
+		loadAllStoreStates: function() {
+			var self = this,
+				radioNames = {};
+			
+			this.$element.find(".storeState").each(function(i, elem) {
+				var $elem = $(elem);
+				if($elem.is("input[type=radio]")) {
+					var name = $elem.prop("name");
+					if(name !== undefined && radioNames[name]) return;
+					else radioNames[name] = true;
+				}
+				self.loadStoreStateFor(elem);
+			});
+		},
 		saveAll: function() {
 			this.savePlayerSource();
 			this.saveProgress();
-			this.savePlaybackRate();
+			this.saveAllStoreStates();
 		},
 		loadAll: function() {
 			this.loadPlayerSource();
 			this.loadProgress();
-			this.loadPlaybackRate();
+			this.loadAllStoreStates();
 		},
 		destroy: function() {
 			$(window).off("unload", this.onunload);
